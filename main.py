fedf6b8f14295802a47e52b831b809b85bbeceac
Harsh Singhal <harshsinghal1972@gmail.com>
2025-05-03 17:24:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # main.py
 
 import ui
-from scan import run_scan
-from cscan import run_cscan
+from scan import scan_schedule
+from cscan import cscan_schedule
 
 if __name__ == "__main__":
     ui.run_ui(run_scan, run_cscan)
